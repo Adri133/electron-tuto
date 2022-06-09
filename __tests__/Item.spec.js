13d83448ad010137abc0f5ffca4ee4db02eb85c0
adrienpignon <pignon.adrien@gmail.com>
2022-06-09 16:04:53 +0200

    Tests item

--- __tests__/Item.spec.js
+++ __tests__/Item.spec.js
@@ -8,6 +8,27 @@ describe('Item class', () => {
     expect(typeof item.getItems).toBe('function')
     expect(item.getItems()).toEqual(Promise.resolve())
   })
+  test('has getItems method which return promise', () => {
+    const item = new Item(db)
+    let spy = jest.spyOn(item, 'addItems').mockImplementation(() => Promise.resolve());
+    expect(typeof item.addItems).toBe('function')
+    expect(typeof item.addItems('test')).toBe('object')
+    expect(item.getItems()).toEqual(Promise.resolve())
+  })
+  test('has getItems method which return promise', () => {
+    const item = new Item(db)
+    let spy = jest.spyOn(item, 'deleteItem').mockImplementation(() => Promise.resolve());
+    expect(typeof item.deleteItem).toBe('function')
+    expect(typeof item.deleteItem('test')).toBe('object')
+    expect(item.getItems()).toEqual(Promise.resolve())
+  })
+  test('has getItems method which return promise', () => {
+    const item = new Item(db)
+    let spy = jest.spyOn(item, 'updateItem').mockImplementation(() => Promise.resolve());
+    expect(typeof item.updateItem).toBe('function')
+    expect(typeof item.updateItem('test')).toBe('object')
+    expect(item.getItems()).toEqual(Promise.resolve())
+  })
   afterAll(() => {
     jest.restoreAllMocks();
   })
