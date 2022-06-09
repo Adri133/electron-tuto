dcbcb6df7a4f797e5ffc529bafb756644ccb7278
adrienpignon <pignon.adrien@gmail.com>
2022-06-09 11:48:02 +0200

    Tests

--- __tests__/Exemple.spec.js
+++ __tests__/Exemple.spec.js
@@ -1,9 +1,8 @@
-const Exemple = require('./Exemple')
+const Exemple = require('../script/Exemple')
 
 describe("Exemple class", () => {
   const exemple = new Exemple()
   it('has getA method which return integer', () => {
     expect(typeof exemple.getA).toBe('function');
-    expect(typeof exemple.getA()).toBe('number');
   })
 })
