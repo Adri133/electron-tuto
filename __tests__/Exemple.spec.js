c11f996e52dbfe3a054d3e651be1540c76177a95
adrienpignon <pignon.adrien@gmail.com>
2022-06-09 12:13:47 +0200

    Test exemple getA

--- __tests__/Exemple.spec.js
+++ __tests__/Exemple.spec.js
@@ -2,7 +2,10 @@ const Exemple = require('../script/Exemple')
 
 describe("Exemple class", () => {
   const exemple = new Exemple()
-  it('has getA method which return integer', () => {
+  test('has getA method which return number to be 3', () => {
     expect(typeof exemple.getA).toBe('function');
+    expect(typeof exemple.getA()).toBe('number')
+    expect(exemple.getA()).toBe(3)
   })
+  
 })
